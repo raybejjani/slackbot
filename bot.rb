ef41cb4b066830b41dbb1b97838aa0eb8ce46ad4
Ann Harter <ann@stripe.com>
2014-10-16 15:40:17 -0700

    added channels for flatterybot to lurk in

--- bot.rb
+++ bot.rb
@@ -11,7 +11,7 @@ require 'yaml'
 # Feel free to change the name of the bot here - this controls what name the bot
 # uses when responding.
 if ARGV.first == 'heroku'
-  bot = HerokuSlackAdapter.new(nick: 'flatterybot', channels: ['animatedgifs', 'bot-testing'], icon_emoji: ':llamablush:')
+  bot = HerokuSlackAdapter.new(nick: 'flatterybot', channels: ['#animatedgifs', '#bot-testing'], icon_emoji: ':llamablush:')
 else
   bot = ChatAdapter::Shell.new(nick: 'flatterybot')
 end
@@ -24,8 +24,13 @@ helpmessage = File.read("help.txt")
 credits = File.read("credits.txt")
 
 
+
 # Do this thing in this block each time the bot hears a message:
 bot.on_message do |message, info|
+  channel = info[:channel]
+  unless ['#animatedgifs', '#aww', '#bot-testing', '#coffee', '#yakshack'].include?(channel)
+    next
+  end  
   # ignore all messages not directed to this bot
   if message.start_with?('compliment')
     if message.start_with?('complimentme') || message.start_with?('compliment me')
